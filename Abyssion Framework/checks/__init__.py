59d14b3e19e59d281a562545f6be8f237e21428f
Lezaey <lezaey999@gmail.com>
2026-03-06 13:36:06 -0800

    Delete Abyssion Framework directory

--- Abyssion Framework/checks/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import django
-from . import web
--- Abyssion Framework/metadata/scanner_info.py
+++
@@ -1,6 +0,0 @@
-SCANNER_INFO = {
-    "name": "Abyssion Framework Security",
-    "version": "1.0.0",
-    "author": "Lezaey",
-    "description": "A security check framework for Django applications."
-}
